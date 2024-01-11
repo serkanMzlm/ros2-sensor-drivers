0fe289589efa419b60e7baedf24b418203d86e06
serkan <serkanmazlum306@gmail.com>
2024-01-11 15:39:46 +0300

    naming errors corrected

--- mpu6050_driver/launch/mpu6050_driver.launch.py
+++ mpu6050_driver/launch/mpu6050_driver.launch.py
@@ -9,12 +9,12 @@ import os
 config = os.path.join(
         get_package_share_directory('mpu6050_driver'),
         'config',
-        'params.yaml'
+        'params.yml'
     )
 
 mpu6050driver_node = Node(
         package='mpu6050_driver',
-        executable='mpu6050_driver',
+        executable='mpu6050_driver_node',
         output="screen",
         emulate_tty=True,
         # respawn=True,
