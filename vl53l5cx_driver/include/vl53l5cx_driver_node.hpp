1fb56b81abdb5cf8f8365427d3675401d5e708e2
serkan <serkanmazlum306@gmail.com>
2024-01-11 16:22:59 +0300

    vl53l5cx file added

---
+++ vl53l5cx_driver/include/vl53l5cx_driver_node.hpp
@@ -0,0 +1,40 @@
+#ifndef __VL53L5CX_DRIVER_NODE_HPP__
+#define __VL53L5CX_DRIVER_NODE_HPP__
+
+#include <memory>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <dlfcn.h>
+#include <string.h>
+
+#include "vl53l5cx_api.hpp"
+#include "vl53l5cx_buffers.hpp"
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/range.hpp"
+
+#define DELAY 10000
+
+using RangeMsg = sensor_msgs::msg::Range;
+
+class VL53L5CXDriver: public rclcpp::Node{
+public:
+    VL53L5CXDriver();
+    int init();
+    void getRange();
+    void reportError(int error, std::string error_info = "Errno");
+private:
+    VL53L5CX_Configuration dev;
+    uint8_t status, isAlive, isReady;
+    VL53L5CX_ResultsData Results;
+
+    rclcpp::Publisher<RangeMsg>::SharedPtr range_pub;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+
+#endif
---
+++ vl53l5cx_driver/src/vl53l5cx_driver_node.cpp
@@ -0,0 +1,54 @@
+#include "vl53l5cx_driver_node.hpp"
+
+VL53L5CXDriver::VL53L5CXDriver(): Node("VL53L5CX_node"){
+    if(init() < 0){
+        exit(-1);
+    }
+}
+
+int VL53L5CXDriver::init(){
+    if(vl53l5cx_comms_init(&dev.platform)){
+        reportError(errno, "Device's initialization failed");
+        return -1;
+    }
+    status = vl53l5cx_is_alive(&dev, &isAlive);
+    if(!isAlive || status){
+        reportError(errno);
+        return -1;
+    }
+    status = vl53l5cx_init(&dev);
+    usleep(DELAY);
+    status = vl53l5cx_set_ranging_frequency_hz(&dev, 20);
+    usleep(DELAY);
+    vl53l5cx_set_resolution(&dev, VL53L5CX_RESOLUTION_4X4);
+    usleep(DELAY);
+    status = vl53l5cx_start_ranging(&dev);
+    vl53l5cx_check_data_ready(&dev, &isReady);
+    usleep(DELAY);
+    return 0;
+}
+
+void VL53L5CXDriver::getRange(){
+    RangeMsg msg;
+    status = vl53l5cx_check_data_ready(&dev, &isReady);
+    // int sum;
+    if(isReady){ 
+        vl53l5cx_get_ranging_data(&dev, &Results);
+        for(int a = 0; a < 16; a++){
+			if(a % 8 == 0) std::cout << "\n";
+            std::cout <<"|" << std::setw(4) << 
+                    Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*a] << " mm|";
+		}
+    }
+}
+
+void VL53L5CXDriver::reportError(int error, std::string error_info ){
+	std::cerr << "Error! " << error_info << ": " << strerror(error); 
+}
+
+int main(int argc, char* argv[]){
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<VL53L5CXDriver>());
+  rclcpp::shutdown();
+  return 0;
+}
